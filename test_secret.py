3c50c1b29767f24796376d9b4b6abe14ec0b3ef4
Marco M <marcomto.mm@gmail.com>
2026-06-23 15:37:51 +0200

    rimosso stampa della api key come log, ora c'è solo la procedura per caricare la chiave.

--- test_secret.py
+++ test_secret.py
@@ -4,4 +4,3 @@ from dotenv import load_dotenv
 load_dotenv("secret.env")
 # Ora la chiave è caricata in memoria, ma non è scritta nel codice!
 api_key = os.getenv("OLLAMA_WEB_SEARCH_KEY")
-print(f"La chiave caricata è: {api_key}")
